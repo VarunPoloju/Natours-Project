590520cf9127278a5540a82202d5c828a9afc6cd
Varun <Varunpoloju966@gmail.com>
2021-11-23 13:37:06 +0530

    Hasing the passwords

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -23,7 +24,27 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm ur password'],
+    validate: {
+      validator: function (ele) {
+        // only works on CREATE & SAVE!!! ex:-   const newUser = await User.create(req.body);
+        // if return value is false we'll get validation error
+        return ele === this.password; //abc(ele) === abc(this.passsword)
+      },
+      message: 'Passwords are not same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // if password is modified -> only run this function
+  if (!this.isModified('password')) return next();
+
+  //  with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // after changing password delete the confirm passwod coz no use
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
